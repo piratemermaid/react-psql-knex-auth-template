037d2bdd0d1e1b9ec30506314ab637993f73f7bb
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2021-02-23 18:13:44 -0500

    Add users table

--- server/db/migrations/20210223090313_initial.js
+++ server/db/migrations/20210223090313_initial.js
@@ -1,7 +1,20 @@
-exports.up = async function (knex) {};
+exports.up = async function (knex) {
+  await knex.schema.createTable("users", (table) => {
+    table.increments("id").unsigned().primary();
+    table.string("username").unique().notNullable();
+    table.string("password").notNullable();
+    table.string("email").unique().notNullable();
+    table.string("sessionId");
+    table.string("verify_email_token");
+    table.timestamp("verify_email_expiry");
+    table.boolean("verified");
+    table.string("reset_pw_token");
+    table.timestamp("reset_pw_expiry");
+  });
+};
 
 module.exports.down = async function (knex) {
-  const tableOrder = [];
+  const tableOrder = ["users"];
   for (let i in tableOrder) {
     await knex.schema.dropTableIfExists(tableOrder[i]);
   }
